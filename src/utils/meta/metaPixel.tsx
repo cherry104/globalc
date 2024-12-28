d24e6a84422f3702242086ca801b5240b4148c4f
cherry104 <fictionalradio1@gmail.com>
2024-12-28 17:16:11 +0530

    Pixel Update

--- src/utils/meta/metaPixel.tsx
+++ src/utils/meta/metaPixel.tsx
@@ -15,8 +15,9 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1344973216493526');
+fbq('init', '1769033777221546');
 fbq('track', 'PageView');
+
           `,
         }}
       />
@@ -25,7 +26,7 @@ fbq('track', 'PageView');
         dangerouslySetInnerHTML={{
           __html: `
 <img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=1344973216493526&ev=PageView&noscript=1" />
+src="https://www.facebook.com/tr?id=1769033777221546&ev=PageView&noscript=1" />
           `,
         }}
       />
